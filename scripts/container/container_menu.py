6511775875adca8e91c0dca1aee4d9b0a69e2aa9
Arief <mariefak22@gmail.com>
2024-06-03 15:53:17 +0200

    modified container script to properly initialise for Linux and WSL2 machines

--- scripts/container/container_menu.py
+++ scripts/container/container_menu.py
@@ -1,10 +1,37 @@
 import os
 
-def menu():
+def system_init() -> str:
+    done = False
+    os_sys = "Linux"
+
+    while done == False:
+        os.system("figlet -t SARAX")
+        print("What host system are you running?")
+        print("1. Linux")
+        print("2. Windows WSL2")
+        
+        choice = int(input("\nChoice (1 - 2): "))
+
+        if choice == 1:
+            done = True
+            print(f"Initialised for {os_sys} host system")
+        elif choice == 2:
+            os_sys = "WSL2"
+            os.system("cd ~ && sed -i 's/^# export LD_LIBRARY_PATH=\/usr\/lib\/wsl\/lib$/export LD_LIBRARY_PATH=\/usr\/lib\/wsl\/lib/' .bashrc")
+            os.system("cd ~ && sed -i 's/^# export LIBVA_DRIVER_NAME=d3d12$/export LIBVA_DRIVER_NAME=d3d12/' .bashrc")
+            print(f"Initialised for {os_sys} host system")
+            done = True
+        else:
+            print("Invalid input")
+
+    return os_sys
+
+def menu(os_sys) -> int:
     done = False
 
     while done == False:
         os.system("figlet -t SARAX")
+        print(f"HOST SYSTEM: {os_sys}")
         print("What would you like to run?")
         print("1. Simulator")
         print("2. Sarax Framework")
@@ -20,6 +47,9 @@ def menu():
             done = True
         else:
             print("Invalid input")
+    
+    return choice
 
 if __name__ == "__main__":
-    menu()
+    os_sys = system_init()
+    menu(os_sys)
--- scripts/docker_container.sh
+++ scripts/docker_container.sh
@@ -51,17 +51,14 @@ if [ $OPTION = 'Run' ]; then
     echo "Attempting to build Docker image"
     if [ $SYSTEM = 'Linux' ]; then
         SIMULATOR_COMMANDS="docker run --name $CONTAINER_NAME --runtime nvidia --gpus all -e NVIDIA_DRIVER_CAPABILITIES=all -it  -v /tmp/.X11-unix:/tmp/.X11-unix -e "DISPLAY=$DISPLAY" -e "QT_X11_NO_MITSHM=1" -e "XAUTHORITY=$XAUTH" --device /dev/dri/ -e "WAYLAND_DISPLAY=$WAYLAND_DISPLAY" -e "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" -e "PULSE_SERVER=$PULSE_SERVER" -p 18570:18570/udp --privileged $IMAGE_ID bash"
-        SARAX_FRAMEWORK_COMMANDS="docker exec -it sarax_container bash && roslaunch m4e_mani_base sarax_plus_sitl.launch"
     fi
 
     if [ $SYSTEM = 'WSL2' ]; then
-        SIMULATOR_COMMANDS="sudo docker run --name sarax_container -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /mnt/wslg:/mnt/wslg -v /usr/lib/wsl:/usr/lib/wsl --device=/dev/dxg -e DISPLAY=$DISPLAY --device /dev/dri/card0 --device /dev/dri/renderD128 -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e PULSE_SERVER=$PULSE_SERVER --gpus all $IMAGE_ID bash"
-        SARAX_FRAMEWORK_COMMANDS="docker exec -it sarax_container bash && roslaunch m4e_mani_base sarax_plus_sitl.launch"
+        SIMULATOR_COMMANDS="docker run --name sarax_container -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /mnt/wslg:/mnt/wslg -v /usr/lib/wsl:/usr/lib/wsl --device=/dev/dxg --device /dev/dri/card0 --device /dev/dri/renderD128 -e DISPLAY=$DISPLAY -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e PULSE_SERVER=$PULSE_SERVER --gpus all $IMAGE_ID bash"
     fi
 
     echo "Running container in a new terminal on $SYSTEM"
     open_new_terminal "$SIMULATOR_COMMANDS"
-    open_new_terminal "$SARAX_FRAMEWORK_COMMANDS"
 fi
 
 if [ $OPTION = 'Start' ]; then
